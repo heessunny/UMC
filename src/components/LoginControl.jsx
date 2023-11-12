7b6e457b44e7e55793cb8841d331fbee0d920049
heesun0419 <80321582+heesun0419@users.noreply.github.com>
2023-11-12 18:56:45 +0900

    Update LoginControl.jsx

--- src/components/LoginControl.jsx
+++ src/components/LoginControl.jsx
@@ -14,7 +14,7 @@ function LoginControl() {
         <li><button onClick={handleToggleClick}>
         {login ? "로그아웃" : "로그인"}
         </button></li>
-        <li>{login ? "로그인을 해주세요!" : "환영합니다!"}
+          <li>{login ? "환영합니다!" : "로그인을 해주세요!"}
         </li>
         </ul>
       </div>
